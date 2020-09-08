3407583d7bdad8fb6fb855a6dff4d1085eb4a099
Joshua <30259508@cityofglacol.ac.uk>
2020-09-08 22:12:20 +0100

    Completed Cash (pset1)

---
+++ pset1/cash.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+float getChangeOwed(void);
+int getCoinsOwed(int cents);
+
+int main(void)
+{
+    float centsOwed = getChangeOwed();
+    int coins = getCoinsOwed(centsOwed);
+    
+    printf("Coins: %i\n", coins);
+}
+
+int getCoinsOwed(int cents)
+{
+    int coins = 0;
+    
+    while (cents > 0)
+    {
+        if (cents >= 25)
+        {
+            cents -= 25;
+            coins++;
+        } 
+        else if (cents >= 10)
+        {
+            cents -= 10;
+            coins++;
+        } else if (cents >= 5)
+        {
+            cents -= 10;
+            coins++;
+        } else {
+            cents--;
+            coins++;
+        }
+    }
+    
+    return coins;
+}
+
+float getChangeOwed(void)
+{
+    float dollars;
+    do
+    {
+        dollars = get_float("Change owed? ");
+    } 
+    while (dollars < 0);
+    
+    return round(100 * dollars);
+}
